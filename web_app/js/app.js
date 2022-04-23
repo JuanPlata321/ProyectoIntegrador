da1b78ea798a4f30b0cde382bbd6af6e626eabb8
Cristian Julian Muñoz Buenahora <cristian.munoz.2020@upb.edu.co>
2022-04-23 18:55:52 -0500

    Implementación del registro para vehículos, usuarios y clientes y definición de la vista de la tabla en la cual se mostrarán las consultas a la base de datos

--- web_app/js/app.js
+++ web_app/js/app.js
@@ -1,7 +1,12 @@
 // Invoke express
 
+const { application } = require('express');
 const express = require('express');
 const app = express();
+
+// Invoke alert
+
+const alert = require('alert');
 
 // Urlencoded to capture form data
 
@@ -29,17 +34,13 @@ app.post("/auth", async(req, res) => {
     const user = req.body.user;
     const pass = req.body.pass;
 
-    // Invoke alert
-
-    const alert = require('alert');
-
     //Invoke crypto
 
     const crypto = require('crypto');
 
     let encryptPassword = crypto.createHash("sha256").update(pass).digest('hex');
 
-    connection.query('SELECT * FROM EMPLEADO WHERE CORREO_ELECTRONICO = ? AND CONTRASENA = ?', [user, encryptPassword], async(error, results)=>{
+    connection.query('SELECT * FROM EMPLEADOS WHERE CORREO_ELECTRONICO = ? AND CONTRASENA = ?', [user, encryptPassword], async(error, results)=>{
         if (results.length > 0){
             if (results[0].ESTADO_EMPLEADO_ID_ESTADO_EMPLEADO == '1'){
                 if (results[0].DEPARTAMENTO_AREA_ID_DEPARTAMENTO_AREA == '1'){
@@ -58,6 +59,127 @@ app.post("/auth", async(req, res) => {
     });
 });
 
+// Display data for items 
+
+app.get('/getVehicles', (req, res) => {
+    connection.query('SELECT * FROM VEHICULOS', (error, results) => {
+        if (error){
+            alert('Ups! Algo ha salido mal al realziar la consulta a la base de datos');
+        }else{
+            res.send(results);
+        }
+    })
+});
+
+// Display data for clients 
+
+app.get('/getClients', (req, res) => {
+    connection.query('SELECT * FROM CLIENTE', (error, results) => {
+        if (error){
+            alert('Ups! Algo ha salido mal al realziar la consulta a la base de datos');
+        }else{
+            res.send(results);
+        }
+    })
+});
+
+// Display data for users
+
+app.get('/getUsers', (req, res) => {
+    connection.query('SELECT * FROM EMPLEADO', (error, results) => {
+        if (error){
+            alert('Ups! Algo ha salido mal al realizar la consulta a la base de datos');
+        }else{
+            res.send(results);
+        }
+    })
+});
+
+// Register items
+
+app.post('/postVehicles', (req, res) => {
+    let data = {marca:req.body.brand, modelo:req.body.model, año:req.body.year, descripcion:req.body.description, tipo:req.body.type, imagen_url:req.body.image, unidades_stock:req.body.unit, valor_unitario:req.body.value};
+    let sql = "INSERT INTO VEHICULOS SET ?";
+    connection.query(sql, data, function (error, results) {
+        res.sendFile('D:/User/Personal/Universidad/2.5 Quinto semestre/Proyecto integrador/Desarrollo/Implementación/Github Repository/web_app/html/registroVehiculo.html');
+        if(error){
+            alert('Ups! Tuvimos problemas al realizar el registro del vehículo, revisa los campos ingresados, seguramente algunos ya están en uso y si el problema persiste contácte con el personal de soporte técnico');
+        }else{
+            alert('Registro realizado exitosamente!');
+        }
+    });
+});
+
+// Register clients
+
+app.post('/postClients', (req, res) => {
+
+    const patternEmail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    const email = req.body.email;
+
+    if (patternEmail.test(email)){
+        let data = {cedula_nit:req.body.identification, nombres:req.body.name, correo_electronico:req.body.email, contacto:req.body.contact, direccion:req.body.address};
+        let sql = "INSERT INTO CLIENTES SET ?";
+        connection.query(sql, data, function (error, results) {
+            res.sendFile('D:/User/Personal/Universidad/2.5 Quinto semestre/Proyecto integrador/Desarrollo/Implementación/Github Repository/web_app/html/registroCliente.html');
+            if(error){
+                alert('Ups! Tuvimos problemas al realizar el registro del cliente, revisa los campos ingresados, seguramente algunos ya están en uso y si el problema persiste contácte con el personal de soporte técnico');
+            }else{
+                alert('Registro realizado exitosamente!');
+            }
+        });
+    }else{
+        alert("La dirección de correo electrónico ingresada no es válida!");
+    }
+});
+
+// Register users
+
+app.post('/postUsers', (req, res) => {
+
+    const area = req.body.area;
+    const status = req.body.status;
+    
+    if (area != 0 && status != 0){
+
+        const patternEmail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        const patternPassword = new RegExp(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/);
+        
+        const pass = req.body.pass;
+        const email = req.body.email; 
+
+
+        if (patternEmail.test(email)){
+            if(pass.length > 8 && patternPassword.test(pass)){
+                //Invoke crypto
+
+                const crypto = require('crypto');
+
+                let encryptPassword = crypto.createHash("sha256").update(pass).digest('hex');
+
+                let data = {cedula:req.body.identification, nombres:req.body.name, apellidos:req.body.lastname, correo_electronico:email, contacto:req.body.contact, direccion:req.body.address, contrasena:encryptPassword, departamento_area_id_departamento_area:area, estado_empleado_id_estado_empleado:status};
+                let sql = "INSERT INTO EMPLEADOS SET ?";
+                connection.query(sql, data, function (error, results) {
+                    res.sendFile('D:/User/Personal/Universidad/2.5 Quinto semestre/Proyecto integrador/Desarrollo/Implementación/Github Repository/web_app/html/registroUsuario.html');
+                    if(error){
+                        alert('Ups! Tuvimos problemas al realizar el registro del cliente, revisa los campos ingresados, seguramente algunos ya están en uso y si el problema persiste contácte con el personal de soporte técnico');
+                    }else{
+                        alert('Registro realizado exitosamente!');
+                    }
+                });
+            }else{
+                alert("La contraseña no es válida! Esta debe tener más de ocho caracteres, incluir al menos una letra en minúscula y una en mayúscula, incluir un número y un caracter especial, entre estos últimos están únicamente los siguientes !@#$%^&*");
+            }
+        }else{
+            alert("La dirección de correo electrónico ingresada no es válida!");
+        }
+    }else{
+        alert("Por favor, selecciona una opción para cada una de las secciones desplegables!");
+    }
+});
+
 // Database connection
 
 const connection = require('./database.js');
+
+module.exports = app;
